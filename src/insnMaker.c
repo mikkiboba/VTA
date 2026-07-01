2ecb50ff229dbd76cf69724d6567885a50f9ec4f
mikkiDev <mikkimail.games@gmail.com>
2026-07-01 11:06:40 +0200

    helper functions for creating UOPs

---
+++ src/insnMaker.c
@@ -0,0 +1,71 @@
+#include "insnMaker.h"
+
+#include <string.h>
+
+
+void memInsn( 
+    VTAGenericInsn* insn_queue, 
+    int insn_idx, 
+    int opcode, 
+    int mem_type, 
+    int sram_base, 
+    uint32_t dram_base,
+    int x_size,
+    int y_size,
+    int x_stride,
+    int pop_prev,
+    int pop_next,
+    int push_prev,
+    int push_next
+) {
+    VTAMemInsn* insn = (VTAMemInsn*)&insn_queue[insn_idx];
+    memset(insn, 0, sizeof(VTAMemInsn));
+
+    insn->opcode = opcode;
+    insn->memory_type = mem_type;
+    insn->sram_base = sram_base;
+    insn->dram_base = dram_base;
+    insn->x_size = x_size;
+    insn->y_size = y_size;
+    insn->x_stride = x_stride;
+    insn->pop_prev_dep = pop_prev;
+    insn->pop_next_dep = pop_next;
+    insn->push_prev_dep = push_prev;
+    insn->push_next_dep = push_next;
+}
+
+
+void gemmInsn(
+    VTAGenericInsn* insn_queue,
+    int insn_idx,
+    int opcode,
+    int reset_reg,
+    int uop_bgn,
+    int uop_end,
+    int iter_out,
+    int iter_in,
+    int dst_factor,
+    int src_factor,
+    int wgt_factor,
+    int pop_prev,
+    int pop_next,
+    int push_prev,
+    int push_next
+){
+    VTAGemInsn* insn = (VTAGemInsn*)&insn_queue[insn_idx];
+    memset(insn, 0, sizeof(VTAGemInsn));
+
+    insn->opcode = opcode;
+    insn->reset_reg = reset_reg;
+    insn->uop_bgn = uop_bgn;
+    insn->uop_end = uop_end;
+    insn->iter_out = iter_out;
+    insn->iter_in = iter_in;
+    insn->dst_factor_out = dst_factor;
+    insn->src_factor_out = src_factor;
+    insn->wgt_factor_out = wgt_factor;
+    insn->pop_prev_dep = pop_prev;
+    insn->pop_next_dep = pop_next;
+    insn->push_prev_dep = push_prev;
+    insn->push_next_dep = push_next;
+}
---
+++ src/insnMaker.h
@@ -0,0 +1,73 @@
+#ifndef INSNMAKER_H
+#define INSNMAKER_H
+
+#include <vta/driver.h>
+#include <vta/hw_spec.h>
+
+
+/*! \brief Create a memory-type micro instruction (UOP). It can be LOAD/STORE.
+    \param insn_queue Queue of micro instructions.
+    \param opcode Operation code of the micro instruction, taken from `hw_spec.h`
+    \param mem_type Type of memory used. Taken from `hw_spec.h`
+    \param sram_base Where to start in the SRAM of the device
+    \param dram_base Where to start in the DRAM of the CPU
+    \param x_size Length of the data
+    \param y_size Heigth of the data
+    \param x_stride How much space between rows of the data
+    \param pop_prev Pop for previous dependecies
+    \param pop_next Pop for next dependecies
+    \param push_prev Push for previous dependecies 
+    \param push_next Push for next dependecies 
+*/
+void memInsn(
+    VTAGenericInsn* insn_queue, 
+    int insn_idx, 
+    int opcode, 
+    int mem_type, 
+    int sram_base, 
+    uint32_t dram_base,
+    int x_size,
+    int y_size,
+    int x_stride,
+    int pop_prev,
+    int pop_next,
+    int push_prev,
+    int push_next
+);
+
+
+/*! \brief Create a memory-type micro instruction (UOP). It can be GEMM (also ALU, actually).
+    \param insn_queue Queue of micro instructions.
+    \param reset_reg Reset value (1 for cleaning, 0 for accumulating)
+    \param uop_bgn Starting index of the UOP to execute from `insn_queue`
+    \param uop_end Ending index (excluded) of the UOP to execute from `insn_queue` 
+    \param iter_out Outer iterations
+    \param iter_in Inner iterations
+    \param dst_factor How much to advance the output at each iteration
+    \param src_factor How much to advance the input at each iteration
+    \param wgt_factor How much to advance the weight at each iteration
+    \param pop_prev Pop for previous dependecies
+    \param pop_next Pop for next dependecies
+    \param push_prev Push for previous dependecies 
+    \param push_next Push for next dependecies 
+*/
+void gemmInsn(
+    VTAGenericInsn* insn_queue,
+    int insn_idx,
+    int opcode,
+    int reset_reg,
+    int uop_bgn,
+    int uop_end,
+    int iter_out,
+    int iter_in,
+    int dst_factor,
+    int src_factor,
+    int wgt_factor,
+    int pop_prev,
+    int pop_next,
+    int push_prev,
+    int push_next
+);
+
+
+#endif
