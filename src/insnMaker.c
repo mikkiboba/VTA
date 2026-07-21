fc1cf91dc1cc98f4faa620049ac17c4e70c61026
mikkiDev <mikkimail.games@gmail.com>
2026-07-21 15:19:58 +0200

    added aluInsn

--- src/insnMaker.c
+++ src/insnMaker.c
@@ -22,15 +22,15 @@ void memInsn(
     VTAMemInsn* insn = (VTAMemInsn*)&insn_queue[insn_idx];
     memset(insn, 0, sizeof(VTAMemInsn));
 
-    insn->opcode = opcode;
-    insn->memory_type = mem_type;
-    insn->sram_base = sram_base;
-    insn->dram_base = dram_base;
-    insn->x_size = x_size;
-    insn->y_size = y_size;
-    insn->x_stride = x_stride;
-    insn->pop_prev_dep = pop_prev;
-    insn->pop_next_dep = pop_next;
+    insn->opcode        = opcode;
+    insn->memory_type   = mem_type;
+    insn->sram_base     = sram_base;
+    insn->dram_base     = dram_base;
+    insn->x_size        = x_size;
+    insn->y_size        = y_size;
+    insn->x_stride      = x_stride;
+    insn->pop_prev_dep  = pop_prev;
+    insn->pop_next_dep  = pop_next;
     insn->push_prev_dep = push_prev;
     insn->push_next_dep = push_next;
 }
@@ -56,17 +56,49 @@ void gemmInsn(
     VTAGemInsn* insn = (VTAGemInsn*)&insn_queue[insn_idx];
     memset(insn, 0, sizeof(VTAGemInsn));
 
-    insn->opcode = opcode;
-    insn->reset_reg = reset_reg;
-    insn->uop_bgn = uop_bgn;
-    insn->uop_end = uop_end;
-    insn->iter_out = iter_out;
-    insn->iter_in = iter_in;
-    insn->dst_factor_out = dst_factor;
-    insn->src_factor_out = src_factor;
-    insn->wgt_factor_out = wgt_factor;
-    insn->pop_prev_dep = pop_prev;
-    insn->pop_next_dep = pop_next;
+    insn->opcode            = opcode;
+    insn->reset_reg         = reset_reg;
+    insn->uop_bgn           = uop_bgn;
+    insn->uop_end           = uop_end;
+    insn->iter_out          = iter_out;
+    insn->iter_in           = iter_in;
+    insn->dst_factor_out    = dst_factor;
+    insn->src_factor_out    = src_factor;
+    insn->wgt_factor_out    = wgt_factor;
+    insn->pop_prev_dep      = pop_prev;
+    insn->pop_next_dep      = pop_next;
+    insn->push_prev_dep     = push_prev;
+    insn->push_next_dep     = push_next;
+}
+
+
+void aluInsn(
+    VTAGenericInsn* insn_queue,
+    int insn_idx,
+    int opcode,
+    int use_imm,
+    int imm,
+    int uop_bgn,
+    int uop_end,
+    int iter_out,
+    int iter_in,
+    int pop_prev,
+    int pop_next,
+    int push_prev,
+    int push_next
+) {
+    VTAAluInsn* insn = (VTAAluInsn*)&insn_queue[insn_idx];
+    memset(insn, 0, sizeof(VTAAluInsn));
+
+    insn->opcode        = opcode;
+    insn->use_imm       = use_imm;
+    insn->imm           = imm;
+    insn->uop_bgn       = uop_bgn;
+    insn->uop_end       = uop_end;
+    insn->iter_out      = iter_out;
+    insn->iter_in       = iter_in;
+    insn->pop_prev_dep  = pop_prev;
+    insn->pop_next_dep  = pop_next;
     insn->push_prev_dep = push_prev;
     insn->push_next_dep = push_next;
 }
--- src/insnMaker.h
+++ src/insnMaker.h
@@ -7,6 +7,7 @@
 
 /*! \brief Create a memory-type micro instruction (UOP). It can be LOAD/STORE.
     \param insn_queue Queue of micro instructions.
+    \param insn_idx Index of the instruction in the queue.
     \param opcode Operation code of the micro instruction, taken from `hw_spec.h`
     \param mem_type Type of memory used. Taken from `hw_spec.h`
     \param sram_base Where to start in the SRAM of the device
@@ -36,8 +37,10 @@ void memInsn(
 );
 
 
-/*! \brief Create a memory-type micro instruction (UOP). It can be GEMM (also ALU, actually).
+/*! \brief Create a micro instruction (UOP). It can be GEMM (also ALU, actually).
     \param insn_queue Queue of micro instructions.
+    \param insn_idx Index of the instruction in the queue.
+    \param opcode Operation code of the micro instruction, taken from `hw_spec.h`
     \param reset_reg Reset value (1 for cleaning, 0 for accumulating)
     \param uop_bgn Starting index of the UOP to execute from `insn_queue`
     \param uop_end Ending index (excluded) of the UOP to execute from `insn_queue` 
@@ -63,6 +66,38 @@ void gemmInsn(
     int dst_factor,
     int src_factor,
     int wgt_factor,
+    int pop_prev,
+    int pop_next,
+    int push_prev,
+    int push_next
+);
+
+
+/*! \brief Create an ALU-type micro instruction (UOP).
+    \param insn_queue Queue of micro instructions.
+    \param insn_idx Index of the instruction in the queue.
+    \param opcode Operation code of the micro instruction, taken from `hw_spec.h`
+    \param use_imm
+    \param imm
+    \param uop_bgn Starting index of the UOP to execute from `insn_queue`
+    \param uop_end Ending index (excluded) of the UOP to execute from `insn_queue` 
+    \param iter_out Outer iterations
+    \param iter_in Inner iterations
+    \param pop_prev Pop for previous dependecies
+    \param pop_next Pop for next dependecies
+    \param push_prev Push for previous dependecies 
+    \param push_next Push for next dependecies 
+*/
+void aluInsn(
+    VTAGenericInsn* insn_queue,
+    int insn_idx,
+    int opcode,
+    int use_imm,
+    int imm,
+    int uop_bgn,
+    int uop_end,
+    int iter_out,
+    int iter_in,
     int pop_prev,
     int pop_next,
     int push_prev,
